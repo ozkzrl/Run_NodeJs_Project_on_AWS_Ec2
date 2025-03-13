1c832b764d9b6ab8d207bf2becb32f18920d3586
ozkzrl <ozkan.zorlu@gmail.com>
2025-03-14 01:02:16 +0300

    Port was changed.

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 // Sample book data
 let books = [
